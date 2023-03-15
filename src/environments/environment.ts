5bacde39537b55b35ee055947afa3e7df6e05a93
khalil <khalillakhdharatc@gmail.com>
2023-03-15 14:31:44 +0100

    login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,12 @@ export const environment = {
   production: false,
   defaultauth: 'fackbackend',
   firebaseConfig: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
+    apiKey: "AIzaSyDN6VbkKgjZQDMbE2izzPUllHX0pOeWjRU",
+    authDomain: "elearnweb-2ece8.firebaseapp.com",
+    projectId: "elearnweb-2ece8",
+    storageBucket: "elearnweb-2ece8.appspot.com",
+    messagingSenderId: "979796747055",
+    appId: "1:979796747055:web:7dbea466ce9ed62a87f7da"
   }
 };
 
